416e7c34e7a17c4a88eff41d3d4a5d8000667555
Amit sonkar <amitkumar6565xzfc@gmail.com>
2026-03-14 10:18:26 +0530

    Add program to multiply two user-input numbers

---
+++ User_input_multiply.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ( ) {
+	
+	int num1, num2, product;
+	
+	printf("First number is :  ");
+	scanf("%d",&num1);
+	
+	printf("Second number is : ");
+	scanf("%d",&num2);
+	
+	product=num1*num2;
+	
+	printf("The product of num1 and num2 is =%d", product);
+	return 0;
+}
