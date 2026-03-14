94d172ff0a0b012bc7bef8f5bc19707abb5fef15
Amit sonkar <amitkumar6565xzfc@gmail.com>
2026-03-14 19:12:58 +0530

    Add print_variables.c to display variable types

---
+++ print_variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ( ) {
+    
+    int a;
+    float b;
+    char c;
+    
+    a=4;
+    b=5.5;
+    c='*';
+    
+    printf("int=%d\n float=%f\n char=%c",a, b, c);
+    
+    return 0;
+}
