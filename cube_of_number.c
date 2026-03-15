56b28beea8a64961638773012873740cf9ce7f85
Amit sonkar <amitkumar6565xzfc@gmail.com>
2026-03-15 08:21:17 +0530

    Add program to calculate the cube of a number

---
+++ cube_of_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ( ) {
+    
+    int n, cube;
+    
+    printf("Enter number :");
+    scanf("%d",&n);
+    
+    cube=n*n*n;
+    
+    printf("cube of %d = %d", n, cube);
+    
+    return 0;
+}
